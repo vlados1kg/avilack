7c996bbcf2732a4b724faf132479f397bf862663
Vladyslav Hliuzitskyi <115615420+gluzik@users.noreply.github.com>
2024-08-12 19:24:26 +0200

    add all

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.querySelector(".button");
+let output = document.querySelector(".main-title");
+let buttonText = document.querySelector(".button-text");
+let count = 1;
+let one = "1.54";
+let two = "2.76";
+let three = "15.02";
+
+output.innerHTML = "5.73";
+
+input.addEventListener("click", () => {
+    if (count === 1) {
+        loading();
+        count++;
+        setTimeout(() => {output.innerHTML = one;}, 1000)
+    } else if (count === 2) {
+        loading();
+        count++;
+        setTimeout(() => {output.innerHTML = two;}, 1000)
+    } else if (count === 3) {
+        loading();
+        setTimeout(() => {output.innerHTML = three;}, 1000)
+    }
+}, 2000)
+
+
+
+function loading () {
+    buttonText.innerHTML = '';
+    buttonText.innerHTML = "<img class=\"loading\" alt=\"\" src=\"./img/loading.png\">";
+    setTimeout(() => {buttonText.innerHTML = "Check";}, 1000)
+}
